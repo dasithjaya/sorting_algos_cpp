825e0a85ab34008a5ec5af19791535d8e33b8a43
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-09-09 08:48:47 +0530

    merge sort

---
+++ merge_sort.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>& arr, int l, int m, int r){
+    /**
+     * arr: the array where the merged elements will be stored
+     * l: left index
+     * m: middle index
+     * r: right index
+     */
+
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<int> L(n1), R(n2);
+
+    for(int i = 0; i < n1; i++){
+        L[i] = arr[l + i];
+    }
+
+    for (int i = 0; i < n2; i++){
+        R[i] = arr[m + 1 + i];
+    }
+
+    int i = 0, j = 0, k = l;
+
+    while(i < n1 && j < n2){
+        if(L[i] <= R[j]){
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while(i < n1){
+        arr[k++] = L[i++];
+    }
+
+    while(j < n2){
+        arr[k++] = R[j++];
+    }
+}
+
+void merge_sort(vector<int>& arr, int l, int r) {
+    if(l < r){
+        int m = l + (r - l) / 2;
+
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main(){
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    merge_sort(arr, 0, arr.size() - 1);
+
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
